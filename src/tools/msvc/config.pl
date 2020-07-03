4a7dbdc8000fb3b153d7109739bb1b19044057c5
Glen A Johnson <glen.johnson@gaj-geospatial.com>
2020-07-03 19:51:56 -0400

    Added vs2019 build

---
+++ src/tools/msvc/config.pl
@@ -0,0 +1,4 @@
+$config->{xslt} = 'D:\Development\op3d_active\libxslt-1.1.28\Win32';
+$conifg->{iconv} = 'D:\Development\op3d_active\libiconv-1.12\MSVC11';
+$config->{xml} = 'D:\Development\op3d_active\libxml2-2.7.7\Win32';
+$config->{zlib} = 'D:\Development\op3d_active\zlib-1.2.8';
